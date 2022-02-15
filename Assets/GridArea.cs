c4b8770c87bdaae4f2a89ffaced36de7d159b1b2
be5her <besher.karam.1998@hotmail.com>
2022-02-15 22:03:10 +0300

    Reading map from csv file

--- Assets/GridArea.cs
+++ Assets/GridArea.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridArea{
+public class GridArea
+{
 
     private int width, height;
     private int[,] gridArray;
@@ -10,29 +11,33 @@ public class GridArea{
     private TextMesh[,] debugArray;
     private Vector3 originPosition;
 
-    public GridArea(int width, int height, float cellSize,Vector3 originPosition){
+    public GridArea(int width, int height, float cellSize, Vector3 originPosition, int[,] GridValues)
+    {
         this.width = width;
         this.height = height;
         this.cellSize = cellSize;
         this.originPosition = originPosition;
-        this.gridArray = new int[width, height];
+        this.gridArray = GridValues;
         this.debugArray = new TextMesh[width, height];
 
-        for (int i = 0; i < width; i++){
-            for (int j = 0; j < height; j++){
-                debugArray[i,j] = createWorldText(null, gridArray[i,j].ToString(), GetWorldPosition(i, j) + new Vector3(cellSize,cellSize) *.5f, 20,"white" , TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWorldPosition(i,j), GetWorldPosition(i ,j+1), Color.white, 100f);
-                Debug.DrawLine(GetWorldPosition(i,j), GetWorldPosition(i+1 ,j), Color.white, 100f);
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                debugArray[i, j] = createWorldText(null, gridArray[i, j].ToString(), GetWorldPosition(i, j) + new Vector3(cellSize, cellSize) * .5f, 20, "white", TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i, j + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i + 1, j), Color.white, 100f);
             }
         }
-        Debug.DrawLine(GetWorldPosition(0,height), GetWorldPosition(width,height), Color.white, 100f);
-        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width,height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
     }
 
-    public TextMesh createWorldText(Transform parent = null, string text = "", Vector3 localPosition = default(Vector3), int fontSize = 40, string color = "white", TextAnchor anchor = TextAnchor.UpperLeft, TextAlignment alignment = TextAlignment.Left, int sortingOrder = 5000){
+    public TextMesh createWorldText(Transform parent = null, string text = "", Vector3 localPosition = default(Vector3), int fontSize = 40, string color = "white", TextAnchor anchor = TextAnchor.UpperLeft, TextAlignment alignment = TextAlignment.Left, int sortingOrder = 5000)
+    {
         GameObject gameObject = new GameObject("world text", typeof(TextMesh));
         Transform transform = gameObject.transform;
-        transform.SetParent(parent,false);
+        transform.SetParent(parent, false);
         transform.localPosition = localPosition;
         TextMesh textMesh = gameObject.GetComponent<TextMesh>();
         textMesh.anchor = anchor;
@@ -44,39 +49,46 @@ public class GridArea{
         return textMesh;
     }
 
-    private Vector3 GetWorldPosition(int x , int y){
-        return new Vector3(x,y) * cellSize + originPosition;
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
     }
 
-    private void GetXY(Vector3 worldPosition,out int x ,out int y){
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
         x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
         y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
     }
 
-    public void SetValue(int x , int y, int value){
-        if (x >= 0 && y >= 0 && x < width && y < height){
-            gridArray[x,y]++;
-            debugArray[x,y].text = gridArray[x,y].ToString();
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y]++;
+            debugArray[x, y].text = gridArray[x, y].ToString();
         }
     }
-    public void SetValue(Vector3 worldPosition , int value){
-        int x,y;
-        GetXY(worldPosition ,out x,out y);
-        SetValue(x,y , value);
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
 
     }
 
 
     // Start is called before the first frame update
-    void Start(){
-        
+    void Start()
+    {
+
     }
 
 
 
     // Update is called once per frame
-    void Update(){
-        
+    void Update()
+    {
+
     }
 
 
--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -1,23 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour{
+public class TestScript : MonoBehaviour
+{
     // Start is called before the first frame update
     GridArea grid;
-    void Start(){
-        grid = new GridArea(132, 60, 1f, new Vector3(-66, -30));
+    void Start()
+    {
+        string currentPath = Directory.GetCurrentDirectory();
+        int[,] mapValues = readMapData(currentPath + "/Assets/Maps/map 1.csv");
+        grid = new GridArea(88, 40, 1.5f, new Vector3(-66, -30), mapValues);
     }
 
     // Update is called once per frame
-    void Update(){
-        if(Input.GetMouseButtonDown(0)){
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
             grid.SetValue(GetMousePositionWorld(), 1);
         }
     }
-    public Vector3 GetMousePositionWorld() {
+
+    public Vector3 GetMousePositionWorld()
+    {
         Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         vec.z = 0f;
         return vec;
+    }
+
+    public int[,] readMapData(string filePath)
+    {
+        int[,] values = new int[88, 40];
+        string[] text = System.IO.File.ReadAllLines(@filePath);
+        string[][] stringValues = new string[40][];
+        for (int i = 0; i < 40; i++)
+        {
+            stringValues[i] = text[i].Split(',');
+        }
+        for (int i = 0; i < 88; i++)
+        {
+            for (int j = 0; j < 40; j++)
+            {
+                values[i, j] = int.Parse(stringValues[j][i]);
+            }
+        }
+        return values;
     }
 }
